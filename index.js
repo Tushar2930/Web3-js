83211724262e65ba32caf9e1c0352a4485924b7b
Tushar Shingane <tusharshingane1234@gmail.com>
2022-11-06 09:36:05 +0000

    started

---
+++ index.js
@@ -0,0 +1,32 @@
+const {
+  Connection,
+  LAMPORTS_PER_SOL,
+  clusterApiUrl,
+  Keypair,
+} = require("@solana/web3.js");
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+(async () => {
+  const keypair = Keypair.generate();
+
+  const airdropSignature = await connection.requestAirdrop(
+    keypair.publicKey,
+    LAMPORTS_PER_SOL
+  );
+
+  const latestBlockHash = await connection.getLatestBlockhash();
+
+  const txn = await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: airdropSignature,
+  });
+
+  console.log({
+    publicKey: keypair.publicKey,
+    privateKey: keypair.privateKey,
+    signature: airdropSignature,
+    txn,
+  });
+})();
